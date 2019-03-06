d4ac5ca05225d583b3300b0a571fee3a81bd59f0
Nannan Gu <396426364@qq.com>
2019-03-06 16:00:03 +1100

    update

--- src/main/java/com/gnn/seckill/redis/prefix/MiaoShaUserKey.java
+++ src/main/java/com/gnn/seckill/redis/prefix/MiaoShaUserKey.java
@@ -5,7 +5,9 @@ package com.gnn.seckill.redis.prefix;
  */
 public class MiaoShaUserKey extends  BasePrefix{
     public static final int TOKEN_EXPIRE = 3600 *24*2; //过期时间2天
-    public static MiaoShaUserKey token = new MiaoShaUserKey(TOKEN_EXPIRE,"tk") ;
+
+    //设置不过期，方便测试
+    public static MiaoShaUserKey token = new MiaoShaUserKey(0,"tk") ;
     public static MiaoShaUserKey getByNickName = new MiaoShaUserKey(0, "nickname");
 
     public MiaoShaUserKey(int expireSeconds ,String prefix) {
